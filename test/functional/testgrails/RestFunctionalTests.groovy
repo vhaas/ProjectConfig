8589de547a7a6e541c5e2a9f827f01a7bda3e89e
Andreas Schillinger <andreas.schillinger@exxeta.com>
2013-03-08 14:04:41 +0100

    Added functional test for Rest Service

--- test/functional/testgrails/RestFunctionalTests.groovy
+++ test/functional/testgrails/RestFunctionalTests.groovy
@@ -1,18 +1,61 @@
 package testgrails
 
 import com.grailsrocks.functionaltest.*
+import org.junit.*
 
 class RestFunctionalTests extends BrowserTestCase {
-    
-	void testGetRoles() {
-		(new Role(name: "Administrator", description: "The most important person in da house")).save()
-		(new Role(name: "Standard User", description: "He's ain't no allowed anything")).save()
-		assertEquals 2, Role.count()
-	  
-		get('http://localhost:8080/TestGrails/rolenames') 
-		assertStatus 200
-	  
-		assertContentContains "Administrator"
-		assertContentContains "Standard User"
-	 }
+	
+	static def url = "http://localhost:8080/TestGrails"
+	
+	def epic
+	def role
+	def project
+	def userStory
+	
+	@Before
+	void fillTestData() {
+		epic = [
+			name: "epicName",
+			description: "epicDescription"
+			]
+		epic = ["epic": epic]
+		
+		role = [
+			name: "roleName",
+			description: "roleDescription"
+			]
+		role = ["role": role]
+		
+		project = [
+			name: "projectName",
+			description: "projectDescription"
+			]
+		project = ["project": project]
+		
+		userStory = [
+			name: "userStoryName",
+			description: "userStoryDescription",
+			goal: "userStoryGoal",
+			benefit: "userStoryBenefit",
+			road_map_id: "",
+			epic_id: "",
+			role_id: ""
+			]
+		userStory = ["user_story": userStory]
+	}	
+	
+	void testPost() {
+		this.properties.each { property ->
+			if (property.key != "url") {
+				def specificUrl = '${url}' + '/${property.value}' + 's'
+				println("The POST was send to: " + specificUrl)
+				post(specificUrl) {
+					body {
+						property.value
+					}
+				}
+				assert status 200
+			}
+		}
+	}
 }
